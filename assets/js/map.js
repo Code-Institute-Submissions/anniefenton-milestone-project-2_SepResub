3b76cbb8ea7f07b4714b9145a5687d943507c53e
anniefenton <anniefentoncontact@gmail.com>
2022-04-11 14:58:44 +0000

    transfer js in hikes.html to separate js file called map.js

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,14 +1,79 @@
 function initMap() {
-    // The location of Uluru
-    const uluru = { lat: -25.344, lng: 131.036 };
-    // The map, centered at Uluru
+    
+    const snowdon = { lat: 53.069, lng: -4.076 };
+    const cheddarGorge = { lat: 51.286, lng: -2.760 }
+    const paveyArk = { lat: 54.461, lng: -3.105 }
+    const quiraing = { lat: 57.640, lng: -6.274}
+    const giantsCauseway = { lat: 55.231, lng: -6.529 }
+    const southDownsWay = { lat: 50.903, lng: -0.514}
+    const carrauntoohil = { lat: 52.0439, lng: -9.685}
+    const penYFan = { lat: 51.883, lng: -3.436 }
+    const benNevis = { lat: 56.796, lng: -5.003}
+    const ludsChurch = { lat: 53.187, lng: -2.020 }
+
     const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
-      center: uluru,
+        zoom: 5,
+        center: paveyArk,
     });
-    // The marker, positioned at Uluru
-    const marker = new google.maps.Marker({
-      position: uluru,
-      map: map,
+
+    const markerSnowdon = new google.maps.Marker({
+        position: snowdon,
+        map: map,
+        title: "Snowdon, Wales, Intermediate"
     });
-  }
+
+    const markerCheddarGorge = new google.maps.Marker({
+        position: cheddarGorge,
+        map: map,
+        title: "Cheddar Gorge, England, Novice"
+    });
+
+    const markerPaveyArk = new google.maps.Marker({
+        position: paveyArk,
+        map: map,
+        title: "Pavey Ark (or Jack's Rake), England, Expert"
+    });
+
+    const markerQuiraing = new google.maps.Marker({
+        position: quiraing,
+        map: map,
+        title: "The Quiraing, Scotland, Intermediate"
+    });
+
+    const markerGiantsCauseway = new google.maps.Marker({
+        position: giantsCauseway,
+        map: map,
+        title: "The Causeway Coast Path, Northern Ireland, Novice"
+    });
+
+    const markerSouthDownsWay = new google.maps.Marker({
+        position: southDownsWay,
+        map: map,
+        title: "The South Downs Way, England, Novice"
+    });
+
+    const markerCarrauntoohil = new google.maps.Marker({
+        position: carrauntoohil,
+        map: map,
+        title: "Carrauntoohil Hike, Republic of Ireland, Expert"
+    });
+
+    const markerPenYFan = new google.maps.Marker({
+        position: penYFan,
+        map: map,
+        title: "Pen Y Fan, Wales, Novice"
+    });
+
+    const markerBenNevis = new google.maps.Marker({
+        position: benNevis,
+        map: map,
+        title: "Ben Nevis, Scotland, Expert"
+    });
+
+    const markerLudsChurch = new google.maps.Marker({
+        position: ludsChurch,
+        map: map,
+        title: "Lud's Chruch, England, Novice"
+    });
+ }
+        
